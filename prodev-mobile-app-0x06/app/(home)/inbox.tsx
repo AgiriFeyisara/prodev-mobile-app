62122d428b6a5806a771a5c09c08165e3c51075b
Feyisara Agiri <mohrenny8@gmail.com>
2026-01-14 16:13:34 +0100

    Added nativewind to the project

---
+++ prodev-mobile-app-0x06/app/(home)/inbox.tsx
@@ -0,0 +1,10 @@
+import {View, Text} from "react-native";
+const Inbox =() => {
+    return(
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default Inbox;
---
+++ prodev-mobile-app-0x06/app/(home)/saved.tsx
@@ -0,0 +1,10 @@
+import {View, Text} from "react-native";
+
+const Saved = () => {
+    return(
+        <View>
+            <Text>Saved screen</Text>
+        </View>
+    )
+}
+export default Saved;
